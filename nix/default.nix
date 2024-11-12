81183f80a2c017638cbd66747e391468f061bbbe
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-11-12 02:10:12 +0000

    Fix Nix builds

--- nix/default.nix
+++ nix/default.nix
@@ -1,12 +1,28 @@
 {
+  lib,
   buildDotnetModule,
   dotnetCorePackages,
 }:
+let
+  fs = lib.fileset;
+  sourceFiles = fs.difference (fs.gitTracked ../.) (
+    fs.unions [
+      (fs.maybeMissing ./result)
+      ../default.nix
+      (fs.fileFilter (file: file.hasExt "nix") ./.)
+      ./npins
+    ]
+  );
+in
 buildDotnetModule {
   pname = "amaurot";
   version = "0.0.1";
 
-  src = ../.;
+  src = fs.toSource {
+    fileset = sourceFiles;
+
+    root = ../.;
+  };
 
   projectFile = "Amaurot.sln";
   nugetDeps = ./deps.nix;
@@ -15,4 +31,6 @@ buildDotnetModule {
   dotnet-runtime = dotnetCorePackages.aspnetcore_8_0;
 
   executables = [ ];
+
+  enableParallelBuilding = false;
 }
