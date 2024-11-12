c6e18546e43c81a908e276a5f7371666cd9bc004
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-11-12 01:55:42 +0000

    Build Anyder Docker image with Nix

---
+++ nix/anyder.nix
@@ -0,0 +1,22 @@
+{
+  pkgs ?
+    let
+      npins = import ./npins;
+    in
+    import npins.nixpkgs { },
+  compressor ? "none",
+}:
+let
+  amaurot = pkgs.callPackage ./. { };
+in
+pkgs.dockerTools.buildLayeredImage {
+  name = "anyder";
+
+  inherit compressor;
+
+  config.command = [
+    (pkgs.lib.getExe amaurot.dotnet-runtime)
+  ];
+
+  tag = "latest";
+}
