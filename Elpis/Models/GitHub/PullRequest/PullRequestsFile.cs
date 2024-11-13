aae0e55db3394beec0635a640d53dc6cd742d5d5
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-11-13 23:00:50 +0000

    Fix deserialising `PulRequestFile`

--- Elpis/Models/GitHub/PullRequest/PullRequestsFile.cs
+++ Elpis/Models/GitHub/PullRequest/PullRequestsFile.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace Elpis.Models.GitHub.PullRequest;
 
 public class PullRequestsFile
 {
+    [JsonPropertyName("filename")]
     public required string FileName { get; init; }
 }
