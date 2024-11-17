5e52938635bfc4081531ab80ffe019ef5c101e1c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-11-17 13:22:51 +0000

    Simplify Nix code

--- Amaurot.Processor/default.nix
+++ Amaurot.Processor/default.nix
@@ -5,29 +5,17 @@
     in
     import npins.nixpkgs { },
 }:
-let
-  fs = pkgs.lib.fileset;
-  sourceFiles = fs.intersection (fs.gitTracked ../.) (
-    (fs.difference ./. (fs.unions [ (fs.fileFilter (file: file.hasExt "nix") ./.) ]))
-  );
-
-  dotnetCorePackages = pkgs.dotnetCorePackages;
-in
 pkgs.buildDotnetModule {
   pname = "amaurot-processor";
   version = "0.0.1";
 
-  src = fs.toSource {
-    fileset = sourceFiles;
+  src = ./.;
 
-    root = ../.;
-  };
-
-  projectFile = "Amaurot.Processor/Amaurot.Processor.csproj";
+  projectFile = "Amaurot.Processor.csproj";
   nugetDeps = ./deps.nix;
 
-  dotnet-sdk = dotnetCorePackages.sdk_9_0;
-  dotnet-runtime = dotnetCorePackages.runtime_9_0;
+  dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
+  dotnet-runtime = pkgs.dotnetCorePackages.runtime_9_0;
 
   executables = [ ];
 }
--- Amaurot.Receiver/default.nix
+++ Amaurot.Receiver/default.nix
@@ -5,29 +5,17 @@
     in
     import npins.nixpkgs { },
 }:
-let
-  fs = pkgs.lib.fileset;
-  sourceFiles = fs.intersection (fs.gitTracked ../.) (
-    (fs.difference ./. (fs.unions [ (fs.fileFilter (file: file.hasExt "nix") ./.) ]))
-  );
-
-  dotnetCorePackages = pkgs.dotnetCorePackages;
-in
 pkgs.buildDotnetModule {
   pname = "amaurot-receiver";
   version = "0.0.1";
 
-  src = fs.toSource {
-    fileset = sourceFiles;
+  src = ./.;
 
-    root = ../.;
-  };
-
-  projectFile = "Amaurot.Receiver/Amaurot.Receiver.csproj";
+  projectFile = "Amaurot.Receiver.csproj";
   nugetDeps = ./deps.nix;
 
-  dotnet-sdk = dotnetCorePackages.sdk_9_0;
-  dotnet-runtime = dotnetCorePackages.aspnetcore_9_0;
+  dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
+  dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_9_0;
 
   executables = [ ];
 }
