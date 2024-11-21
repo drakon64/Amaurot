1982ca37082afd38f2c3108697bc509c73dcf165
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-11-21 21:47:31 +0000

    Add Firestore attributes

--- Amaurot.Processor/Models/Amaurot/SavedPlan.cs
+++ Amaurot.Processor/Models/Amaurot/SavedPlan.cs
@@ -1,10 +1,22 @@
+using Google.Cloud.Firestore;
+
 namespace Amaurot.Processor.Models.Amaurot;
 
+[FirestoreData]
 internal class SavedPlan
 {
+    [FirestoreProperty]
     public required string PullRequest { get; init; }
+
+    [FirestoreProperty]
     public required string Sha { get; init; }
+
+    [FirestoreProperty]
     public required string Directory { get; init; }
+
+    [FirestoreProperty]
     public required string Workspace { get; init; }
+
+    [FirestoreProperty]
     public required byte[] PlanOut { get; init; }
 }
