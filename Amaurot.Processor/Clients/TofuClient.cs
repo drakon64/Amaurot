4103b0e40d2a96c9999280977416399a28cf47a0
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-11-26 20:33:41 +0000

    Set SSH private key permissions within Cloud Run

--- Amaurot.Processor/Clients/TofuClient.cs
+++ Amaurot.Processor/Clients/TofuClient.cs
@@ -28,27 +28,16 @@ internal static class TofuClient
         processStartInfo.ArgumentList.Add("-no-color");
 
         string? planOutPath = null;
-        string? tempSshKeyPath = null;
 
         switch (executionType)
         {
             case ExecutionType.Init:
-                var sshKeyPath = Environment.GetEnvironmentVariable("SSH_KEY_PATH");
+                const string sshKeyPath = "/ssh/ssh-private-key";
 
-                if (sshKeyPath is not null)
+                if (File.Exists(sshKeyPath))
                 {
-                    tempSshKeyPath = Path.GetTempFileName();
-
-                    await File.WriteAllBytesAsync(
-                        tempSshKeyPath,
-                        await File.ReadAllBytesAsync(sshKeyPath)
-                    );
-#pragma warning disable CA1416
-                    File.SetUnixFileMode(tempSshKeyPath, UnixFileMode.UserRead);
-#pragma warning restore CA1416
-
                     processStartInfo.Environment["GIT_SSH_COMMAND"] =
-                        $"ssh -i {tempSshKeyPath} -o StrictHostKeyChecking=no";
+                        $"ssh -i {sshKeyPath} -o StrictHostKeyChecking=no";
                 }
 
                 break;
@@ -78,9 +67,6 @@ internal static class TofuClient
         using var tofu = Process.Start(processStartInfo);
 
         await tofu!.WaitForExitAsync();
-
-        if (tempSshKeyPath is not null)
-            File.Delete(tempSshKeyPath);
 
         var stdout = await tofu.StandardOutput.ReadToEndAsync();
         var stderr = await tofu.StandardError.ReadToEndAsync();
