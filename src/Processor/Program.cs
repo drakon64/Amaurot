4e988515113a17b85071c92cd0e7247a4badfb3a
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-12 00:46:42 +0100

    Add arguments to root command

--- src/Processor/Program.cs
+++ src/Processor/Program.cs
@@ -2,21 +2,6 @@
 
 var plan = new Command("plan", "Perform an OpenTofu plan run");
 
-var repository = new Argument<string>("repository")
-{
-    Description = "The GitHub repository to pull from",
-};
-
-var pullRequest = new Argument<ulong>("pull request")
-{
-    Description = "The pull request number to comment on",
-};
-
-var commit = new Argument<string>("commit")
-{
-    Description = "The commit hash to perform a plan run against",
-};
-
 var path = new Option<DirectoryInfo>("--path", "-p")
 {
     Description =
@@ -29,14 +14,10 @@ var varsFile = new Option<FileInfo[]>("--vars-file")
         "Path to a vars file to be used during the plan run. Can be specified multiple times.",
 };
 
-var apply = new Command("apply", "Perform an OpenTofu apply run");
-
-plan.Arguments.Add(repository);
-plan.Arguments.Add(pullRequest);
-plan.Arguments.Add(commit);
-
 plan.Options.Add(path);
 plan.Options.Add(varsFile);
+
+var apply = new Command("apply", "Perform an OpenTofu apply run");
 
 plan.SetAction(parseResult =>
 {
@@ -52,6 +33,25 @@ plan.SetAction(parseResult =>
 });
 
 var rootCommand = new RootCommand("Sample app for System.CommandLine");
+
+var repository = new Argument<string>("repository")
+{
+    Description = "The GitHub repository to pull from",
+};
+
+var pullRequest = new Argument<ulong>("pull request")
+{
+    Description = "The pull request number to comment on",
+};
+
+var commit = new Argument<string>("commit")
+{
+    Description = "The commit hash to perform a run against",
+};
+
+rootCommand.Arguments.Add(repository);
+rootCommand.Arguments.Add(pullRequest);
+rootCommand.Arguments.Add(commit);
 rootCommand.Subcommands.Add(plan);
 rootCommand.Subcommands.Add(apply);
 
