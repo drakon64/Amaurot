00e5cc075e09a53f9420236d0ccc4ecc9edc706b
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-12 11:11:46 +0100

    Make command-line options apply to the root command

--- src/Processor/Program.cs
+++ src/Processor/Program.cs
@@ -1,43 +1,6 @@
 ﻿using System.CommandLine;
 
-var plan = new Command("plan", "Perform an OpenTofu plan run");
-
-var path = new Option<DirectoryInfo>("--path", "-p")
-{
-    Description =
-        "The subdirectory in the repository to perform the plan run in. Defaults to the repository root.",
-};
-
-var varsFile = new Option<FileInfo[]>("--vars-file")
-{
-    Description =
-        "Path to a vars file to be used during the plan run. Can be specified multiple times.",
-};
-
-plan.Options.Add(path);
-plan.Options.Add(varsFile);
-
-var apply = new Command("apply", "Perform an OpenTofu apply run");
-
-plan.SetAction(parseResult =>
-{
-    foreach (var file in parseResult.GetValue(varsFile))
-    {
-        foreach (var line in File.ReadLines(file.FullName))
-        {
-            Console.WriteLine(line);
-        }
-    }
-
-    return 0;
-});
-
-var rootCommand = new RootCommand("Sample app for System.CommandLine");
-
-var repository = new Argument<string>("repository")
-{
-    Description = "The GitHub repository to pull from",
-};
+var repository = new Argument<string>("repository") { Description = "The repository to pull from" };
 
 var pullRequest = new Argument<ulong>("pull request")
 {
@@ -49,10 +12,26 @@ var commit = new Argument<string>("commit")
     Description = "The commit hash to perform a run against",
 };
 
+var plan = new Command("plan", "Perform an OpenTofu plan run");
+var apply = new Command("apply", "Perform an OpenTofu apply run");
+
+var path = new Option<string>("--path", "-p")
+{
+    Description = "The subdirectory in the repository to perform the run in",
+};
+
+var varsFile = new Option<string[]>("--vars-file")
+{
+    Description = "Path to a vars file to be used during the run",
+};
+
+var rootCommand = new RootCommand("Sample app for System.CommandLine");
 rootCommand.Arguments.Add(repository);
 rootCommand.Arguments.Add(pullRequest);
 rootCommand.Arguments.Add(commit);
 rootCommand.Subcommands.Add(plan);
 rootCommand.Subcommands.Add(apply);
+rootCommand.Options.Add(path);
+rootCommand.Options.Add(varsFile);
 
 return rootCommand.Parse(args).Invoke();
