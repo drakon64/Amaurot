129b312f4e4bc051328bc9d94d9d385c01e7a6e9
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-12 12:52:05 +0100

    Begin support for OpenTofu plans

---
+++ src/Processor/Client/OpenTofu/OpenTofuClient.cs
@@ -0,0 +1,3 @@
+namespace Amaurot.Processor.Client.OpenTofu;
+
+internal partial class OpenTofuClient(DirectoryInfo workingDirectory, FileInfo[]? varFiles) { }
---
+++ src/Processor/Client/OpenTofu/Plan.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics;
+
+namespace Amaurot.Processor.Client.OpenTofu;
+
+internal partial class OpenTofuClient
+{
+    internal async Task<PlanOutput> Plan()
+    {
+        var outFile = Path.GetTempFileName();
+
+        var processStartInfo = new ProcessStartInfo
+        {
+            WorkingDirectory = workingDirectory.FullName,
+        };
+
+        processStartInfo.ArgumentList.Add("plan");
+
+        if (varFiles != null)
+            foreach (var varFile in varFiles)
+                processStartInfo.ArgumentList.Add($"-var-file={varFile.FullName}");
+
+        processStartInfo.ArgumentList.Add("-detailed-exitcode");
+        processStartInfo.ArgumentList.Add("-input=false");
+        processStartInfo.ArgumentList.Add("-no-color");
+        processStartInfo.ArgumentList.Add("-concise");
+        processStartInfo.ArgumentList.Add($"-out={outFile}");
+
+        using var tofu = Process.Start(processStartInfo);
+
+        if (tofu == null)
+            throw new Exception(); // TODO: Useful exception
+
+        await tofu.WaitForExitAsync();
+
+        if (!SucceededExitCodes.Contains(tofu.ExitCode))
+            return new PlanOutput
+            {
+                StandardOutput = await tofu.StandardOutput.ReadToEndAsync(),
+                StandardError = await tofu.StandardError.ReadToEndAsync(),
+            };
+
+        var planOut = await File.ReadAllBytesAsync(outFile);
+        File.Delete(outFile);
+
+        return new SuccessfulPlanOutput
+        {
+            Out = planOut,
+            StandardOutput = await tofu.StandardOutput.ReadToEndAsync(),
+            StandardError = await tofu.StandardError.ReadToEndAsync(),
+        };
+    }
+
+    private static readonly int[] SucceededExitCodes = [0, 2];
+
+    internal class PlanOutput
+    {
+        public required string StandardOutput { get; init; }
+        public required string StandardError { get; init; }
+    }
+
+    internal sealed class SuccessfulPlanOutput : PlanOutput
+    {
+        public required byte[] Out { get; init; }
+    }
+}
