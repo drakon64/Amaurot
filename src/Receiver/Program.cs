84281a2bb5e25c1ea99766a3e2faa6072862a752
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-20 16:25:36 +0100

    Minimal Receiver

--- src/Receiver/Program.cs
+++ src/Receiver/Program.cs
@@ -1,5 +1,3 @@
-using System.Text.Json.Serialization;
-
 namespace Amaurot.Receiver;
 
 internal static class Program
@@ -7,41 +5,7 @@ internal static class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateSlimBuilder(args);
-
-        builder.Services.ConfigureHttpJsonOptions(options =>
-        {
-            options.SerializerOptions.TypeInfoResolverChain.Insert(
-                0,
-                AppJsonSerializerContext.Default
-            );
-        });
-
         var app = builder.Build();
-
-        var sampleTodos = new Todo[]
-        {
-            new(1, "Walk the dog"),
-            new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-            new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-            new(4, "Clean the bathroom"),
-            new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2))),
-        };
-
-        var todosApi = app.MapGroup("/todos");
-        todosApi.MapGet("/", () => sampleTodos);
-        todosApi.MapGet(
-            "/{id}",
-            (int id) =>
-                sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-                    ? Results.Ok(todo)
-                    : Results.NotFound()
-        );
-
         app.Run($"http://*:{Environment.GetEnvironmentVariable("PORT")}");
     }
 }
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext { }
