fd3a8a4eeb29f3ebf6dc8b1f997217e8928143ee
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-21 22:32:05 +0100

    Configure GitHub webhook service for Receiver

--- src/Receiver/Program.cs
+++ src/Receiver/Program.cs
@@ -1,3 +1,7 @@
+using Amaurot.Receiver.Webhook;
+using Octokit.Webhooks;
+using Octokit.Webhooks.AspNetCore;
+
 namespace Amaurot.Receiver;
 
 internal static class Program
@@ -5,7 +9,14 @@ internal static class Program
     internal static void Main()
     {
         var builder = WebApplication.CreateSlimBuilder();
+        builder.Services.AddSingleton<WebhookEventProcessor, PullRequestWebhookEventProcessor>();
+
         var app = builder.Build();
+
+        app.MapGitHubWebhooks(
+            secret: Environment.GetEnvironmentVariable("AMAUROT_GITHUB_WEBHOOK_SECRET")
+        );
+
         app.Run($"http://*:{Environment.GetEnvironmentVariable("PORT")}");
     }
 }
