d9531d1aedce5f98da552966e1f25587a947bd81
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-30 22:34:01 +0000

    Dispose of `amaurot.json` reader and simplify schema

--- src/Receiver/Client/Amaurot/AmaurotClient.cs
+++ src/Receiver/Client/Amaurot/AmaurotClient.cs
@@ -3,32 +3,32 @@ using System.Text.Json.Serialization;
 
 namespace Amaurot.Receiver.Client.Amaurot;
 
-internal sealed partial class AmaurotClient(FileInfo amaurotJson)
+internal sealed partial class AmaurotClient
 {
-    private readonly string[] _amaurotPaths = (
-        from deployment in JsonSerializer
-            .Deserialize<AmaurotJson>(
-                amaurotJson.OpenText().ReadToEnd(),
-                KebabCaseLowerSourceGenerationContext.Default.AmaurotJson
-            )!
-            .Deployments
-        select deployment.Value.Path.TrimEnd('/')
-    )
-        .Distinct()
-        .ToArray();
+    private readonly string[] _amaurotPaths;
 
-    internal sealed class AmaurotJson
+    internal AmaurotClient(FileInfo amaurotJson)
     {
-        public required IReadOnlyDictionary<string, Deployment> Deployments { get; init; }
+        using var json = amaurotJson.OpenText();
 
-        internal sealed class Deployment
-        {
-            public required string Path { get; init; }
-            public string[]? VarFiles { get; init; }
-        }
+        _amaurotPaths = (
+            from deployment in JsonSerializer.Deserialize<IReadOnlyDictionary<string, Deployment>>(
+                json.ReadToEnd(),
+                KebabCaseLowerSourceGenerationContext.Default.IReadOnlyDictionaryStringDeployment
+            )!
+            select deployment.Value.Path.TrimEnd('/')
+        )
+            .Distinct()
+            .ToArray();
+    }
+
+    internal sealed class Deployment
+    {
+        public required string Path { get; init; }
+        public string[]? VarFiles { get; init; }
     }
 
     [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.KebabCaseLower)]
-    [JsonSerializable(typeof(AmaurotJson))]
+    [JsonSerializable(typeof(IReadOnlyDictionary<string, Deployment>))]
     private sealed partial class KebabCaseLowerSourceGenerationContext : JsonSerializerContext;
 }
