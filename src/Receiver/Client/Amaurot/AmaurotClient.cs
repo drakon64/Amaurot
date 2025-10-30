7eca457ef3ba454af825875d55bb53082be21157
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-30 22:05:54 +0000

    Filter changed paths to ones defined in `amaurot.json`

--- src/Receiver/Client/Amaurot/AmaurotClient.cs
+++ src/Receiver/Client/Amaurot/AmaurotClient.cs
@@ -1,9 +1,22 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Amaurot.Receiver.Client.Amaurot;
 
-internal sealed partial class AmaurotClient
+internal sealed partial class AmaurotClient(FileInfo amaurotJson)
 {
+    private readonly string[] _amaurotPaths = (
+        from deployment in JsonSerializer
+            .Deserialize<AmaurotJson>(
+                amaurotJson.OpenText().ReadToEnd(),
+                KebabCaseLowerSourceGenerationContext.Default.AmaurotJson
+            )!
+            .Deployments
+        select deployment.Value.Path.TrimEnd('/')
+    )
+        .Distinct()
+        .ToArray();
+
     internal sealed class AmaurotJson
     {
         public required IReadOnlyDictionary<string, Deployment> Deployments { get; init; }
--- src/Receiver/Client/Amaurot/GetPullRequestPaths.cs
+++ src/Receiver/Client/Amaurot/GetPullRequestPaths.cs
@@ -4,19 +4,18 @@ namespace Amaurot.Receiver.Client.Amaurot;
 
 internal sealed partial class AmaurotClient
 {
-    internal static async Task<string[]> GetPullRequestPaths(
-        string repo,
-        long number,
-        long installationId
-    )
+    internal async Task<string[]> GetPullRequestPaths(string repo, long number, long installationId)
     {
         return (
             from file in await GitHubClient.ListPullRequestFiles(repo, number, installationId)
             where
-                file.Filename.EndsWith(".tf")
-                || file.Filename.EndsWith(".tfvars")
-                || file.Filename.EndsWith(".tftpl")
-                || file.Filename == ".terraform.lock.hcl"
+                _amaurotPaths.Contains(file.Filename[..(file.Filename.LastIndexOf('/') - 1)])
+                && (
+                    file.Filename.EndsWith(".tf")
+                    || file.Filename.EndsWith(".tfvars")
+                    || file.Filename.EndsWith(".tftpl")
+                    || file.Filename == ".terraform.lock.hcl"
+                )
             select file.Filename[..(file.Filename.LastIndexOf('/') - 1)]
         )
             .Distinct()
